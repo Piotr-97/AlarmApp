3d09ed475bb40c6277b00c8118407b309b4a406a
Piotr Karolak <karolak.piotr.012@gmail.com>
2022-01-15 21:48:47 +0100

    Added ModelMapper HomeService, HomeController and LawyerService

--- src/main/java/com/example/demo/controler/HomeController.java
+++ src/main/java/com/example/demo/controler/HomeController.java
@@ -1,9 +1,7 @@
 package com.example.demo.controler;
 
-
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class LawyerContoller {
-
+public class HomeController {
 }
--- src/main/java/com/example/demo/repo/CompanyClientRepository.java
+++ src/main/java/com/example/demo/repo/CompanyClientRepository.java
@@ -2,7 +2,10 @@ package com.example.demo.repo;
 
 import com.example.demo.repo.entity.CompanyClient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface CompanyClientRepository extends JpaRepository<CompanyClient,Long> {
 
 }
