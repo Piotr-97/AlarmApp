5cded3d3a489ac25ef82caab9d5e990a960ed3a9
Piotr Karolak <karolak.piotr.012@gmail.com>
2022-02-09 23:36:51 +0100

    Added method in ClientService to get Lawyers which one are assigned to Client

--- src/main/java/com/example/demo/controler/ClientController.java
+++ src/main/java/com/example/demo/controler/ClientController.java
@@ -1,11 +1,15 @@
 package com.example.demo.controler;
 
 import com.example.demo.repo.ClientRepository;
+import com.example.demo.repo.entity.Lawyer;
 import com.example.demo.service.ClientService;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 
 
 @Controller
@@ -16,6 +20,12 @@ public class ClientController {
     private final ClientRepository clientRepository;
     private final ClientService clientService;
     private final ModelMapper modelMapper;
+
+
+    @GetMapping("/Clientlawyers/{id}")
+    public List<Lawyer> getClientLawyers(@RequestParam Long id){
+        return clientService.getClientlawyers(id);
+    }
 
 
 
--- src/main/java/com/example/demo/service/ClientService.java
+++ src/main/java/com/example/demo/service/ClientService.java
@@ -1,8 +1,26 @@
 package com.example.demo.service;
 
 
+import com.example.demo.repo.ClientRepository;
+import com.example.demo.repo.entity.Lawyer;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class ClientService {
+
+    private final ModelMapper modelMapper;
+    private final ClientRepository clientRepository;
+
+
+   public List<Lawyer> getClientlawyers(@RequestParam Long id){
+        return clientRepository.findAllLawyerByClient(id);
+   }
+
 }
